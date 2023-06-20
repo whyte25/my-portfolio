7622609b4fbf26cdd0c77c41c476d88facf7853b
whyte25 <fasifeoluwa3@gmail.com>
2023-06-20 12:16:36 +0100

    update

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'aszofoz6',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,6 @@
+import header from './header'
+import about from './about'
+import stack from './stack'
+import portfolio from './portfolio'
+import experience from './experience'
+export const schemaTypes = [header, experience, portfolio, stack, about]
